f3891b3515657ef83bd42aa4d2fae88d1a589cda
Zheka Doe <jdoe058@gmail.com>
2024-12-09 09:24:08 +0300

    refactor: delete double code

--- src/main/java/servlets/CurrencyServlet.java
+++ src/main/java/servlets/CurrencyServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import services.CurrencyService;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
@@ -17,8 +16,6 @@ public class CurrencyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         ResponseDto responseDto = service.findByCode(req.getPathInfo());
         resp.setStatus(responseDto.status());
         resp.getWriter().write(responseDto.message());
